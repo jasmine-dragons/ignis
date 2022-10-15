58996309b20d9a5ff5fb3e828e98c17938ede301
Nishant Balaji <nishantb1130@gmail.com>
2022-10-15 16:27:36 -0700

    border + county highlights

--- src/components/MapStyles/BorderStyles.js
+++ src/components/MapStyles/BorderStyles.js
@@ -1,13 +1,13 @@
 import MAP_STYLE from './MapStyles.json';
 
-const usCounties = {
-  type: 'geojson',
-  data: 'data/counties.geojson',
-};
+// const usCounties = {
+//   type: 'geojson',
+//   data: 'data/counties.geojson',
+// };
 
-const fillLayer = {
+export const fillLayer = {
   id: 'county-fill',
-  source: 'us-counties',
+  'source-layer': 'original',
   type: 'fill',
   paint: {
     'fill-outline-color': '#0040c8',
@@ -16,13 +16,37 @@ const fillLayer = {
   },
 };
 
-const lineLayer = {
+export const lineLayer = {
   id: 'county-outline',
-  source: 'us-counties',
+  source: 'county-fill',
+  'source-layer': 'original',
+
   type: 'line',
   paint: {
     'line-width': 2,
-    'line-color': '#969696',
+    'line-color': '#555',
+  },
+};
+
+export const countiesFillLayer = {
+  id: 'counties',
+  type: 'fill',
+  'source-layer': 'original',
+  paint: {
+    'fill-outline-color': 'rgba(0,0,0,0.1)',
+    'fill-color': 'rgba(0,0,0,0.03)',
+  },
+};
+// Highlighted county polygons
+export const highlightFillLayer = {
+  id: 'counties-highlighted',
+  type: 'fill',
+  source: 'counties',
+  'source-layer': 'original',
+  paint: {
+    'fill-outline-color': '#484896',
+    'fill-color': '#6e599f',
+    'fill-opacity': 0.75,
   },
 };
 
@@ -31,9 +55,9 @@ const BorderStyles = {
   ...MAP_STYLE,
   sources: {
     ...MAP_STYLE.sources,
-    ['us-counties']: usCounties,
+    // ['us-counties']: usCounties,
   },
-  layers: [...MAP_STYLE.layers, fillLayer, lineLayer],
+  layers: [...MAP_STYLE.layers],
 };
 
 export default BorderStyles;
--- src/components/MapWrapper/index.jsx
+++ src/components/MapWrapper/index.jsx
@@ -1,11 +1,16 @@
-import { useRef } from 'react';
-import ReactMapGL, { Layer } from 'react-map-gl';
+import { useRef, useState, useCallback, useMemo } from 'react';
+import ReactMapGL, { Layer, Source } from 'react-map-gl';
 import { config } from '../../lib/config';
 import { locations } from '../../lib/constants';
 import ControlLayer from '../ControlLayer';
 import bbox from '@turf/bbox';
 
-import BorderStyles from '../MapStyles/BorderStyles';
+import BorderStyles, {
+  countiesFillLayer,
+  highlightFillLayer,
+  lineLayer,
+  fillLayer,
+} from '../MapStyles/BorderStyles';
 // const style = 'mapbox://styles/acmhacks/cl99a828b006m14pdvvisfm7w';
 
 const MapWrapper = () => {
@@ -27,6 +32,21 @@ const MapWrapper = () => {
     }
   };
 
+  const [hoverInfo, setHoverInfo] = useState(null);
+
+  const onHover = useCallback(event => {
+    const county = event.features && event.features[0];
+    setHoverInfo({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat,
+      countyName: county && county.properties.COUNTY,
+    });
+  }, []);
+
+  const selectedCounty = (hoverInfo && hoverInfo.countyName) || '';
+
+  const filter = useMemo(() => ['in', 'COUNTY', selectedCounty], [selectedCounty]);
+
   return (
     <div>
       <ReactMapGL
@@ -39,11 +59,20 @@ const MapWrapper = () => {
         projection="globe"
         mapboxAccessToken={config.MAPBOX_TOKEN}
         mapStyle={BorderStyles}
-        interactiveLayerIds={['county-fill']}
+        interactiveLayerIds={['counties']}
+        onMouseMove={onHover}
         onClick={onClick}
       >
         <ControlLayer />
         {/* <Layer type="custom" renderingMode="3d" /> */}
+        <Source type="vector" url="mapbox://mapbox.82pkq93d">
+          <Layer beforeId="waterway-label" {...countiesFillLayer} />
+          <Layer beforeId="waterway-label" {...highlightFillLayer} filter={filter} />
+        </Source>
+        <Source type="vector" url="mapbox://mapbox.82pkq93d">
+          <Layer beforeId="waterway-label" {...fillLayer} />
+          <Layer beforeId="waterway-label" {...lineLayer} />
+        </Source>
       </ReactMapGL>
     </div>
   );
