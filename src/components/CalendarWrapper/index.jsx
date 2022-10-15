81f05ea69883b4ee5fb4ae79f565ca55587164ba
Nishant Balaji <nishantb1130@gmail.com>
2022-10-15 13:57:44 -0700

    update font global

--- src/components/CalendarWrapper/index.jsx
+++ src/components/CalendarWrapper/index.jsx
@@ -3,11 +3,17 @@ import 'react-calendar/dist/Calendar.css';
 import styles from './style.module.scss';
 
 const CalendarWrapper = () => {
-    return (
-        <>
-            <Calendar className={styles.calendar}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Calendar
+        className={styles.calendar}
+        calendarType="US"
+        next2Label={null}
+        prev2Label={null}
+        tileClassName={styles.calendarTile}
+      />
+    </>
+  );
+};
 
 export default CalendarWrapper;
