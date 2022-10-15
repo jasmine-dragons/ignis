a1bffe50391ba671638d699e5a7f03608a0cc5ca
Nishant Balaji <nishantb1130@gmail.com>
2022-10-15 13:14:03 -0700

    calendar initial

---
+++ src/components/CalendarWrapper/index.jsx
@@ -0,0 +1,12 @@
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const CalendarWrapper = () => {
+    return (
+        <>
+            <Calendar/>
+        </>
+    )
+}
+
+export default CalendarWrapper;
--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -1,7 +1,8 @@
+import CalendarWrapper from '../components/CalendarWrapper';
 import style from '../styles/pages/Calendar.module.scss';
 
 const CalendarPage = () => {
-  return <div className={style.container}>Calendar</div>;
+  return <div className={style.container}><CalendarWrapper /></div>;
 };
 
 export default CalendarPage;
