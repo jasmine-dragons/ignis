3af8c835237b3b925a39078a06134f89939d42fc
Nishant Balaji <nishantb1130@gmail.com>
2022-10-15 17:20:23 -0700

    popup

--- src/components/MapWrapper/index.jsx
+++ src/components/MapWrapper/index.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState, useCallback, useMemo } from 'react';
-import ReactMapGL, { Layer, Source } from 'react-map-gl';
+import ReactMapGL, { Layer, Popup, Source } from 'react-map-gl';
 import { config } from '../../lib/config';
 import { locations } from '../../lib/constants';
 import ControlLayer from '../ControlLayer';
@@ -11,15 +11,16 @@ import BorderStyles, {
   lineLayer,
   fillLayer,
 } from '../MapStyles/BorderStyles';
-// const style = 'mapbox://styles/acmhacks/cl99a828b006m14pdvvisfm7w';
+import GeocoderControl from '../GeocoderControl';
 
 const MapWrapper = () => {
   const mapRef = useRef();
+  const [hoverInfo, setHoverInfo] = useState(null);
+  const selectedCounty = (hoverInfo && hoverInfo.countyName) || '';
 
   const onClick = event => {
     const feature = event.features[0];
     if (feature) {
-      // calculate the bounding box of the feature
       const [minLng, minLat, maxLng, maxLat] = bbox(feature);
 
       mapRef.current.fitBounds(
@@ -32,8 +33,6 @@ const MapWrapper = () => {
     }
   };
 
-  const [hoverInfo, setHoverInfo] = useState(null);
-
   const onHover = useCallback(event => {
     const county = event.features && event.features[0];
     setHoverInfo({
@@ -43,10 +42,7 @@ const MapWrapper = () => {
     });
   }, []);
 
-  const selectedCounty = (hoverInfo && hoverInfo.countyName) || '';
-
   const filter = useMemo(() => ['in', 'COUNTY', selectedCounty], [selectedCounty]);
-
   return (
     <div>
       <ReactMapGL
@@ -59,10 +55,11 @@ const MapWrapper = () => {
         projection="globe"
         mapboxAccessToken={config.MAPBOX_TOKEN}
         mapStyle={BorderStyles}
-        interactiveLayerIds={['counties']}
+        interactiveLayerIds={['counties', 'county-fill']}
         onMouseMove={onHover}
         onClick={onClick}
       >
+        <GeocoderControl mapboxAccessToken={config.MAPBOX_TOKEN} position="top-left" />
         <ControlLayer />
         {/* <Layer type="custom" renderingMode="3d" /> */}
         <Source type="vector" url="mapbox://mapbox.82pkq93d">
@@ -73,6 +70,18 @@ const MapWrapper = () => {
           <Layer beforeId="waterway-label" {...fillLayer} />
           <Layer beforeId="waterway-label" {...lineLayer} />
         </Source>
+        {hoverInfo && selectedCounty && (
+          <Popup
+            longitude={hoverInfo.longitude}
+            latitude={hoverInfo.latitude}
+            offset={[0, -10]}
+            closeButton={false}
+            closeOnClick={false}
+            className="county-info"
+          >
+            {selectedCounty}
+          </Popup>
+        )}
       </ReactMapGL>
     </div>
   );
