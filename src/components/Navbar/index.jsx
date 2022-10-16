c33e80848088a4d67ec6540409067dab03956a4f
Faris Ashai <farisashai@gmail.com>
2022-10-16 03:47:20 -0700

    Please deploy

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,23 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './style.module.scss';
 
+import style from './style.module.scss';
 const Navbar = () => (
-  <>
-    <div className={styles.navbar}>
-      <div className={styles.navbarLeft}>
-        <Link href="/">
-          <a href="replace">
-            <Image src="/header.png" width={62} height={32} />
-          </a>
-        </Link>
-      </div>
-      <nav className={styles.navbarRight}>
-        <Link href="/home">Map</Link>
-      </nav>
+  <div className={style.navbar}>
+    <div className={style.navbarLeft}>
+      <Link href="/">
+        <a href="replace">
+          <Image src="/header.png" width={62} height={32} />
+        </a>
+      </Link>
     </div>
-    <div className={styles.rainbow} />
-  </>
+    <nav className={style.navbarRight}>
+      <Link href="/home">Map</Link>
+    </nav>
+  </div>
 );
 
 export default Navbar;
